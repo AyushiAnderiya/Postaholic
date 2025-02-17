8003b47a3c660ecbd26d410caa5445dd3400d76c
AyushiAnderiya <ayushianderiya14@gmail.com>
2025-02-17 16:19:51 +0530

    creating profile page

--- app.js
+++ app.js
@@ -42,8 +42,18 @@ app.get('/login', async(req,res)=>{
     res.render("login")
 })
 app.get('/profile', isLoggedIn, async(req,res)=>{
-    console.log(req.user);
-    res.render("login");
+   let user= await userModel.findOne({email:req.user.email}).populate("posts");
+    res.render("profile",{user});
+})
+app.post('/post', isLoggedIn, async(req,res)=>{
+  let user= await userModel.findOne({email:req.user.email});
+  let post=await postModel.create({
+    user:user._id,
+    content:req.body.content
+  })
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
 })
 app.post('/login', async(req,res)=>{
         let{email,password}=req.body;
@@ -53,11 +63,11 @@ app.post('/login', async(req,res)=>{
         bcrypt.compare(password,user.password,(err,result)=>{
             if(result){
                 let token=jwt.sign({email:email,userid:user._id},"sshhhhh");
-                  res.cookie("token",token);
-                res.status(400).send("succesful login");  
+                res.cookie("token",token);
+                res.redirect("/profile");  
             } 
             else{
-              res.redirect("login");  
+              res.redirect("/login");  
             } 
 
 
@@ -73,7 +83,7 @@ app.get('/logout',(req,res)=>{
 
 function isLoggedIn(req,res,next){
     if(req.cookies.token===""){
-      res.send("you must be logged in");  
+      res.redirect("/login");  
     } 
     else{
       let data=jwt.verify(req.cookies.token,"sshhhhh");  
